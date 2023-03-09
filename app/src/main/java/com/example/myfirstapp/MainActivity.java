bf7e80c013df3ffb142b122a523bec743dd371db
mgcllee <mgcllee@gmail.com>
2023-03-09 15:23:26 +0900

    상호작용 버튼 추가
    버튼이 클릭되었을 때 함수를 호출하여 텍스트박스 내용을 변경함

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = findViewById(R.id.pushMeBTN);
+        btn.setOnClickListener(this);
+
+//        TextView tv = findViewById(R.id.snumTextView);
+//        tv.setText("Just Loaded");
+    }
+
+    @Override
+    public void onClick(View view) {
         TextView tv = findViewById(R.id.snumTextView);
-        tv.setText("Just Loaded");
+        tv.setText("BTN ON!");
     }
 }
